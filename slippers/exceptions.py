d41986fab4a292b4ef7e519a7f6097cfd3ed9ea9
ben-qr <ben.gregory@quickrelease.co.uk>
2024-07-01 08:41:16 +0100

    feat: raise SlippersException on bad JSON arg

---
+++ slippers/exceptions.py
@@ -0,0 +1,10 @@
+class SlippersError(Exception):
+    def __init__(self, msg: str, argument: str, reraised: str, component: str):
+        self.msg = msg
+        self.argument = argument
+        self.reraised = reraised
+        self.component = component
+        super().__init__(msg)
+
+    def __str__(self):
+        return f"{self.msg} (\"{self.component}.{self.argument}\"): {self.reraised}"
